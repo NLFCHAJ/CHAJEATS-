b8fdd7fc4de050f6e5fa0db190a244cbfc6b7b40
CHAJEATS <99wn5ktjms@privaterelay.appleid.com>
2026-09-24 15:27:21 -0500

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "CHAJEats",
+  description: "Food delivery made simple",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
